a3cd5c5dd53443e5708f7c636833875a5e559b87
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-09-13 20:16:13 +0200

    Updates

---
+++ precache-manifest.73350c70de66aa0ce5c7648cf8a8f7e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc711d97f675a2337eceacf86987d040",
+    "url": "/react_calculator/index.html"
+  },
+  {
+    "revision": "12d1c2987551f635314f",
+    "url": "/react_calculator/static/css/main.d599f7d6.chunk.css"
+  },
+  {
+    "revision": "ea0b8f42882ad127977a",
+    "url": "/react_calculator/static/js/2.bd647a7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_calculator/static/js/2.bd647a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d1c2987551f635314f",
+    "url": "/react_calculator/static/js/main.19a07aa5.chunk.js"
+  },
+  {
+    "revision": "aefe11394b776b891622",
+    "url": "/react_calculator/static/js/runtime-main.0e3b7e6e.js"
+  }
+]);
